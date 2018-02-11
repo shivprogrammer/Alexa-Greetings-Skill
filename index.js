36c809cb00cb20042a607ba22f452d4e3c6a5da2
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-11 00:06:07 -0800

    added build response function

--- index.js
+++ index.js
@@ -3,4 +3,33 @@ exports.handler = function(event, context) {
   var request = event.request;
 
   // request type
+
+  if (request.type === "LaunchRequest") {
+
+  }
+  else if (request.type === "IntentRequest") {
+
+  }
+  else if (request.type === "SessionEndedRequest") {
+
+  }
+  else {
+    context.fail("Unknown intent type");
+  }
+}
+
+function buildResponse(options) {
+  var response = {
+    version: "1.0",
+    response: {
+      outputSpeech: {
+        type: "PlainText",
+        text: options.speechText
+      },
+      shouldEndSession: options.endSession
+    }
+
+    if (options.speechText) {
+      
+    }
 }
